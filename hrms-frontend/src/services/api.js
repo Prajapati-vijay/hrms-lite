3ab6642954fd83e2561704933997a46f34090536
vijay-prajapati <vijayprajapati260263@gmail.com>
2026-03-27 14:22:39 +0530

    Update api.js

--- hrms-frontend/src/services/api.js
+++ hrms-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hrms-lite-1-t72g.onrender.com/api/",
+  baseURL: "https://vijay286.pythonanywhere.com/api/",
 });
 
 export default API;
